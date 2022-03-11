92ea6e530c8f306baf891dfea84c09bebed52aea
YHK9 <YHK9@eurofins.com>
2022-03-12 04:32:35 +0530

    Added the Arithmetic and primary expressions

---
+++ Expressions/code.js
@@ -0,0 +1,23 @@
+// Arithmetic operator
+
+1/2
+i++
+i=2
+i *2
+
+
+// Primary expressions
+// under this category go (variable refrences, literals and constraints)
+2
+0.002
+"something is looking good!"
+true
+false
+this //this is used to refer current object
+undefined
+i // where i can be variable or the constant
+
+
+
+
+
